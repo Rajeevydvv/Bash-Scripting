791fd86a54a320aabe4cbb0655323f10ff2263ed
Rajeev Yadav <Rajeevofficial3@gmail.com>
2026-05-29 08:57:04 +0000

    Created variables script

---
+++ variables.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# This a world of a fictional character Jethalal.
+<< comment
+Anything
+written
+will not be executed.
+comment
+
+name="Jetha"
+
+echo "Name is $name and Date is $(Date)"
+
+
+
+
