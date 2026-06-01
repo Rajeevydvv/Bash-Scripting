9b0f05630b068bf5ae39cc87d6b45183e44a1396
Rajeev-Yadav <Rajeevofficial3@gmail.com>
2026-06-01 11:28:20 +0000

    Sample code for function in Bash

---
+++ function.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# Define a simple function
+print_args() {
+    echo "First argument: $1"
+    echo "Second argument: $2"
+}
+
+# Call the function with two arguments
+print_args "Hello" "World"
+
